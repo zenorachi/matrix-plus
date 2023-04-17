1949df14603f6d8bf18656eff8d0ab1d854f1217
Zatanna Lyte <zatannal@at-l8.msk.21-school.ru>
2023-04-17 18:11:33 +0300

    S21Matrix: setters probably done

--- class-methods/arithmetic-methods/mul_matrix.cc
+++ class-methods/arithmetic-methods/mul_matrix.cc
@@ -2,14 +2,11 @@
 
 void S21Matrix::MulMatrix(const S21Matrix& other) {
   if (cols_ == other.rows_) {
-    int n = rows_;
-    int m = other.rows_;
-    int k = other.cols_;
-    S21Matrix tmpMatrix(rows_, k);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < k; j++) {
-        for (int l = 0; l < m; l++) {
-          tmpMatrix.matrix_[i][j] += matrix_[i][l] * other.matrix_[l][j];
+    S21Matrix tmpMatrix(rows_, other.cols_);
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < other.cols_; j++) {
+        for (int k = 0; k < other.rows_; k++) {
+          tmpMatrix.matrix_[i][j] += matrix_[i][k] * other.matrix_[k][j];
         }
       }
     }
